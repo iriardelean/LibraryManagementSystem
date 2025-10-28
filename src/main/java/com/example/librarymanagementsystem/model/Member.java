5fd6507a2eb03fa8c79e0e78d7b57a77dda707d9
hudea <hudealex1@gmail.com>
2025-10-28 03:22:50 +0200

    completed classes and enums

---
+++ src/main/java/com/example/librarymanagementsystem/model/Member.java
@@ -0,0 +1,59 @@
+package com.example.librarymanagementsystem.model;
+
+import java.util.List;
+
+public class Member {
+    private String Id;
+    private String Name;
+    private String LibraryId;
+    private List<Reservation> reservetions;
+    private List<Loan> loans;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLibraryId() {
+        return LibraryId;
+    }
+
+    public void setLibraryId(String libraryId) {
+        LibraryId = libraryId;
+    }
+
+    public List<Reservation> getReservetions() {
+        return reservetions;
+    }
+
+    public void setReservetions(List<Reservation> reservetions) {
+        this.reservetions = reservetions;
+    }
+
+    public List<Loan> getLoans() {
+        return loans;
+    }
+
+    public void setLoans(List<Loan> loans) {
+        this.loans = loans;
+    }
+
+    public Member(String id, String name, String libraryId, List<Reservation> reservetions, List<Loan> loans) {
+        Id = id;
+        Name = name;
+        LibraryId = libraryId;
+        this.reservetions = reservetions;
+        this.loans = loans;
+    }
+}
---
+++ src/main/java/com/example/librarymanagementsystem/model/Reservation.java
@@ -0,0 +1,59 @@
+package com.example.librarymanagementsystem.model;
+
+import java.util.Date;
+
+public class Reservation {
+    private String Id;
+    private String memberId;
+    private String ReadableitemId;
+    private Date date;
+    private ReservationStatus ReservationStatus;
+
+    public Reservation(String id, String memberId, String readableitemId, Date date, ReservationStatus ReservationStatus) {
+        Id = id;
+        this.memberId = memberId;
+        ReadableitemId = readableitemId;
+        this.date = date;
+        this.ReservationStatus = ReservationStatus;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getReadableitemId() {
+        return ReadableitemId;
+    }
+
+    public void setReadableitemId(String readableitemId) {
+        ReadableitemId = readableitemId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public ReservationStatus getStatus() {
+        return ReservationStatus;
+    }
+
+    public void setStatus(ReservationStatus ReservationStatus) {
+        this.ReservationStatus = ReservationStatus;
+    }
+}
